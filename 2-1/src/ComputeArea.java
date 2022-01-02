fdb94a31bace4d174068e9c9ac81351884dd7d8f
li11lililj <91314803+li11lililj@users.noreply.github.com>
2022-01-02 23:47:08 +0800

    Add files via upload

---
+++ 2-1/src/ComputeArea.java
@@ -0,0 +1,12 @@
+public class ComputeArea {
+	public static void main(String[] args) {
+		double r;
+		double area;
+		r = 20;
+		area = r * r * 3.14159;
+		System.out.println("The area for the circle of r " + r + " is " + area);
+	}                                          
+	         
+	
+
+}
